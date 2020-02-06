1d972a0caf7f3ee7212a498b2e62e06280a47152
Emily Taylor <etaylor21@ufl.edu>
2020-02-06 17:34:54 -0500

    TUMBLIN DRIFT ADDED

--- gnv_streams_DO_drift_correction_20200204.R
+++ gnv_streams_DO_drift_correction_20200204.R
@@ -332,3 +332,82 @@ write_csv(data_clean2, path = "C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emi
 #Start Time: 16:20 EST/ 21:20 UTC
 #End Date: 8/19/19
 #End Time: 9:30 EST/ 14:30 UTC
+
+
+
+#####TUMBLIN CREEK#####
+
+#Pre-Calibration# 
+#Start Date: 2/13/19
+#Start Time: 11:15 EST/ 16:15 UTC
+#End Date: 2/14/19
+#End Time: 11:20 EST/ 16:20 UTC
+
+#Post-Calibration# 
+#Start Date: 6/19/19
+#Start Time: 14:48 EST/ 18:48 UTC
+#End Date: 6/21/19
+#End Time: 13:14 EST/ 17:14 UTC
+
+
+TUM_precal = read_csv('C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emily Taylor/GNV Streams/Data/Metabolism/Metab Test 2/Calibration_Files/TUM441_20190213_20190214_SN20472625.csv', col_types = cols(
+  DateTime_UTC = col_datetime(format = "%y-%m-%d %H:%M:%S"), 
+  DOconc_mgL = col_double(),
+  Temp_C = col_double()))
+
+bubble_start_DO = TUM_precal %>% 
+  filter(DateTime_UTC >= as_datetime('2019-02-13 16:19:06') & DateTime_UTC <=  as_datetime('2019-02-14 16:19:06')) %>% 
+  summarise(mean(DOconc_mgL), mean(Temp_C))
+
+bubble_start_DO
+
+#Record Bubble Start Avg DO: 8.61 mg/L
+#Record Bubble Start Avg Temp: 21.4 C
+#DO look-up value: 8.85 mg/L
+
+
+TUM_postcal = read_csv('C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emily Taylor/GNV Streams/Data/Metabolism/Metab Test 2/Calibration_Files/TUM441_20190424_20190710_SN20472625.csv', col_types = cols(
+  DateTime_UTC = col_datetime(format = "%y-%m-%d %H:%M:%S"), 
+  DOconc_mgL = col_double(),
+  Temp_C = col_double()))
+
+bubble_end_DO = TUM_postcal %>% 
+  filter(DateTime_UTC >= as_datetime('2019-06-19 18:20:06') & DateTime_UTC <=  as_datetime('2019-06-21 17:30:06')) %>% 
+  summarise(mean(DOconc_mgL), mean(Temp_C))
+
+bubble_end_DO
+
+#Record Bubble End Avg DO: 8.72 mg/L
+#Record Bubble End Avg Temp: 21.4 C
+#DO look-up value: 8.85 mg/L
+
+###This would be the better method to process data in that I woud run the drift correction first and then upload to StreamPULSE for data cleaning###
+#data = read_csv('C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emily Taylor/GNV Streams/Data/Metabolism/Metab Test 2/Calibration_Files/HOGNW16th_20190214_20190814_SN20472627_summary.csv', col_types = cols(
+  #Date = col_date(format = "%y-%m-%d"),
+  #Time_UTC = col_time(format = "%H:%M:%S"), 
+  #DOconc_mgL = col_double(),
+  #Temp_C = col_double()))
+
+#data_clean = dr_factor(data, corrFactor = corrFac, dateVar = Date, timeVar = Time_UTC, keepDateTime = TRUE) %>%
+  #dr_correctOne(sourceVar = DOconc_mgL, cleanVar = DO_Corr, calVal = 8.72, calStd = 8.85, factorVar = corrFac)
+
+#data_clean
+
+#write_csv(data_clean, path = "C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emily Taylor/GNV Streams/Data/Metabolism/Metab Test 2/Calibration_Files/HOGNW16th_20190214_20190814_SN20472627_summary_clean.csv", 
+          #na = 'NA', col_names = TRUE)
+
+
+data2 = read_csv('C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emily Taylor/GNV Streams/Data/Metabolism/Metab Test 2/Calibration_Files/HOGUP_sensordata_15min.csv', col_types = cols(
+  Date = col_date(format = "%y-%m-%d"),
+  Time_UTC = col_time(format = "%H:%M:%S"), 
+  DOconc_mgL = col_double()))
+
+data_clean2 = dr_factor(data2, corrFactor = corrFac, dateVar = Date, timeVar = Time_UTC, keepDateTime = TRUE) %>%
+  dr_correctOne(sourceVar = DOconc_mgL, cleanVar = DO_Corr, calVal = 8.72, calStd = 8.85, factorVar = corrFac)
+
+data_clean2
+
+write_csv(data_clean2, path = "C:/Users/Emily/Dropbox (UFL)/AJR Lab/Students/Emily Taylor/GNV Streams/Data/Metabolism/Metab Test 2/Calibration_Files/HOGNW16th_20190214_20190814_SN20472627_summary_clean2_15min.csv", 
+          na = 'NA', col_names = TRUE)
+
+
